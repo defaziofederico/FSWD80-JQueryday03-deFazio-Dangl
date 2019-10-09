cd767b6a4b6d46150f319f5b476864ea5c38ad00
chriswizz <christian.dangl1@gmail.com>
2019-10-09 13:25:19 +0200

    Advanced

--- Advanced/myscript.js
+++ Advanced/myscript.js
@@ -1,24 +1,14 @@
 $(document).ready(function(){
 	$(document).on("mouseover",function(event){
-	$("#moveme").css({"margin-left":event.pageX-128,"margin-top":event.pageY-128})
-
-
+		$("#moveme").css({"margin-left":event.pageX-128,"margin-top":event.pageY-128})
 
 	});
-
-	$("#moveme").on("click",function(){
-		$(this).animate({
-						"paddingTop": '+=300'
-		},2000,"linear",function() {$(this).fadeOut(1000).fadeIn(1000)    
-
+		$("#moveme").on("click",function(event){
+			$(this).animate({
+				"bottom": 0 + 128,
+				"left": 0,
+			},
+			2000,"linear",function() {$(this).fadeOut(1000).fadeIn(1000)
+			});
 		});
-
-
-
-
-
-	});
-
-
-
 });
